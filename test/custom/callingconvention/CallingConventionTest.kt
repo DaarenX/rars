7d93e49e44a7e667ae73be3a37aed0999c2c210c
Daaren <killxcrafter@gmail.com>
2024-02-22 18:47:43 +0100

    fix test

--- test/custom/callingconvention/CallingConventionTest.kt
+++ test/custom/callingconvention/CallingConventionTest.kt
@@ -2,14 +2,15 @@ package custom.callingconvention
 
 import rars.Globals
 import rars.Launch
+import rars.custom.callingconvention.CallingConventionErrorType
 
 fun main() {
     Launch.main(arrayOf("test/custom/callingconvention/callersave/unnecessary_save_before_call.asm"))
     assert(Globals.callingConventionChecker.exceptionList.size == 1)
-    assert(Globals.callingConventionChecker.exceptionList.getOrNull(0)?.message == "t0(0): saved without being written")
+    assert(Globals.callingConventionChecker.exceptionList.getOrNull(0) == CallingConventionErrorType.CALLER_SAVE_SAVE_WITHOUT_WRITE)
     Launch.main(arrayOf("test/custom/callingconvention/callersave/unnecessary_save_after_call.asm"))
     assert(Globals.callingConventionChecker.exceptionList.size == 1)
-    assert(Globals.callingConventionChecker.exceptionList.getOrNull(0)?.message == "t0(1): saved and restored without calling a function in between")
+    assert(Globals.callingConventionChecker.exceptionList.getOrNull(0) == CallingConventionErrorType.CALLER_SAVE_SAVED_AND_RESTORED_WITHOUT_CALLING_FUNCTION)
     println(Globals.callingConventionChecker.exceptionList)
 
 }
